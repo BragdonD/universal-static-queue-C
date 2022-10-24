f54a2a08f65870f902a5e190a62f697f9a6a28eb
Thomas DUCLOS <thomasduclos1012@gmail.com>
2022-10-24 10:28:52 +0900

    add comment to the queue

--- inc/static_queue.h
+++ inc/static_queue.h
@@ -11,14 +11,21 @@ typedef struct static_queue {
     int rear_index;
 } static_queue;
 
+///Function to handle the memory
+
 static_queue* staticQueue_init(size_t elem_size, int max_size);
+bool staticQueue_resize(static_queue* queue, int size);
+void staticQueue_free(static_queue* queue, void (*free_data)(void*));
+
+///Function to manipulate the queue
+
 unsigned staticQueue_size(static_queue* queue);
 bool staticQueue_isEmpty(static_queue* queue);
-void staticQueue_empty(static_queue* queue);
+void staticQueue_empty(static_queue* queue, void (*free_data)(void*));
 void* staticQueue_front(static_queue* queue);
 void* staticQueue_end(static_queue* queue);
 bool staticQueue_push(static_queue* queue, void* elem);
 void* staticQueue_pop(static_queue* queue);
-bool resize(static_queue* queue, int size);
+
 
 #endif
--- src/static_queue.c
+++ src/static_queue.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include "static_queue.h"
 
+/// @brief Function to create a static empty queue. It is not callable by the end user
+/// @return Return an allocated pointer. NULL in case of error.
 static_queue* create_pStaticQueue() {
     static_queue* temp = NULL;
     temp = malloc(sizeof(static_queue));
-    return temp;
+    return temp; ///Handle the malloc error in the caller function.
 }
 
+/// @brief Function to create a queue with a maximum size of element.
+/// @param elm_size The type size to allocated the array.
+/// @param max_size The size of the array to be allocated.
+/// @return A pointer toward a created queue. Call staticQueue_free
 static_queue* staticQueue_init(size_t elm_size, int max_size) {
     static_queue* temp = create_pStaticQueue();
     if(temp == NULL) return NULL;
-
     temp->arr = calloc(max_size, elm_size);
     if(temp->arr == NULL) return NULL;
     temp->elem_size = elm_size;
@@ -21,27 +26,51 @@ static_queue* staticQueue_init(size_t elm_size, int max_size) {
     return temp;  
 }
 
+/// @brief Function to get the size of a queue
+/// @param queue A pointer toward a static queue.
+/// @return the size of the queue.
 unsigned staticQueue_size(static_queue* queue) {
     return queue->rear_index - queue->front_index;
 }
 
+/// @brief Function to test if the queue is empty. 
+/// @param queue A pointer toward a static queue.
+/// @return true or false.
 bool staticQueue_isEmpty(static_queue* queue) {
     return staticQueue_size(queue) == 0;
 }
 
-void staticQueue_empty(static_queue* queue) {
+/// @brief Function to reset the queue
+/// @param queue a pointer toward a static queue.
+/// @param free_data a function to free the data. You can pass NULL if you do want or need to free it.
+void staticQueue_empty(static_queue* queue, void (*free_data)(void*)) {
+    int len = staticQueue_size(queue);
+    if(free_data != NULL)
+        for(int i=0; i<len; i++) {
+            free_data(staticQueue_pop(queue));
+        }
     queue->front_index = 0;
     queue->rear_index = 0;
 }
 
+/// @brief Function to get the data at the front of the queue
+/// @param queue a pointer toward a static queue
+/// @return a pointer toward the data at the front of the queue
 void* staticQueue_front(static_queue* queue) {
     return (char*)(queue->arr) + (queue->front_index * queue->elem_size);
 }
 
+/// @brief Function to get the data at the end of the queue
+/// @param queue a pointer toward a static queue
+/// @return a pointer toward the data at the end of the queue
 void* staticQueue_end(static_queue* queue) {
     return (char*)(queue->arr) + ((queue->rear_index - 1) * queue->elem_size);
 }
 
+/// @brief Function to push an element at the end of a static queue.
+/// @param queue A pointer toward a static queue.
+/// @param elem A pointer toward the data to be inserted.
+/// @return true on success. false if the queue is full.
 bool staticQueue_push(static_queue* queue, void* elem) {
     int insert_index = queue->rear_index >= queue->max_size ? 0 : queue->rear_index;
     if(insert_index == queue->front_index && !staticQueue_isEmpty(queue)) return false;
@@ -50,6 +79,9 @@ bool staticQueue_push(static_queue* queue, void* elem) {
     return true;
 }
 
+/// @brief Function to remove an element at the end of a static queue.
+/// @param queue A pointer toward a static queue.
+/// @return pointer toward the data to be retrieve. NULL on error.
 void* staticQueue_pop(static_queue* queue) {
     void *pop_elem = NULL;
     if(staticQueue_isEmpty(queue)) return NULL;
@@ -58,10 +90,28 @@ void* staticQueue_pop(static_queue* queue) {
     return pop_elem;
 }
 
-bool resize(static_queue* queue, int size) {
+/// @brief Function to resize the queue. Does not handle the memory of your data.
+/// @param queue a pointer toward the queue to be resize.
+/// @param size the new max size of the queue
+/// @return true on success.
+bool staticQueue_resize(static_queue* queue, int size) {
     void *arr = NULL;
     arr = realloc(queue->arr, size * queue->elem_size);
     if(arr == NULL) return false;
     queue->arr = arr;
     return false;
+}
+
+/// @brief Function to be call at the end to free the queue data.
+/// @param queue a pointer toward the queue.
+/// @param free_data a function to free you data. Can be NULL if you do not want or need to free it.
+void staticQueue_free(static_queue* queue, void (*free_data)(void*)) {
+    if(queue == NULL) return;
+    int len = staticQueue_size(queue);
+    if(free_data != NULL)
+        for(int i=0; i<len; i++) {
+            free_data(staticQueue_pop(queue));
+        }
+    free(queue->arr);
+    free(queue);
 }
